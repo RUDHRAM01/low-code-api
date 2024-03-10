823dc9130d5b35514d0201fe54249ec21e2c044b
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-10 16:58:51 +0530

    Add project creation functionality and display projects on Admin Dashboard

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -2,27 +2,42 @@ import React, { useEffect } from "react";
 import SideBar from "../components/common/SideBar";
 import AddIcon from "../assets/add.svg";
 import Search from "../assets/search.svg";
+import LinkIcon from '../assets/link.svg';
 import LaunchIcon from "../assets/launch.svg";
 import Mongo from "../assets/mongo.svg";
 import { getProjectApi } from "../api/get/GetProjectApi";
+import { createProjectApi } from "../api/post/CreateProjectApi";
+import { Error } from "../apiRes/ErrorCatch";
 
 function AdminDashboard() {
   const [isCreate, setIsCreate] = React.useState(false);
   const [config, setConfig] = React.useState({
     name: "",
-    database: "",
+    dbUrl: "",
+    isLoading: false,
   });
   const [projects, setProjects] = React.useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await getProjectApi();
-      setProjects(response.data);
-      console.log(response.data);
+      setProjects(response.data.message);
+      console.log(response.data.message);
     };
     fetchData();
   }, []);
 
+  const createProject = async (e) => {
+    e.preventDefault();
+    try {
+      setConfig((pre) => ({ ...pre, isLoading: true }));
+      await createProjectApi(config);
+      setConfig((pre) => ({ ...pre, isLoading: false }));
+    } catch (err) {
+      setConfig((pre) => ({ ...pre, isLoading: false }));
+      Error(err);
+    }
+  };
 
   return (
     <div className="dashboard">
@@ -59,29 +74,63 @@ function AdminDashboard() {
               />
               <div></div>
             </div>
+            <div>
+              <span className="project-title">Projects</span>
+            </div>
+            <div></div>
+            <div className="projects">
+              {projects.map((project, index) => (
+                <div key={index} className="project">
+                  <div className="project-name">
+                    <p>{project.name}</p>
+                    <img src={LinkIcon} alt="link" className="icon" />
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         )}
         {isCreate && (
           <div className="create-project-form">
             <div className="create-title">
-              <span>Create Project</span><img src={LaunchIcon} alt="create" className="icon"/>
+              <span>Create Project</span>
+              <img src={LaunchIcon} alt="create" className="icon" />
             </div>
-            <hr className="hr"/>
-            <form>
+            <hr className="hr" />
+            <form onSubmit={createProject}>
               <div className="form">
                 <div className="form-group">
                   <label htmlFor="name">Project Name</label>
-                  <input type="text" id="name" placeholder="name" required onChange={(e)=>(setConfig((pre)=>({...pre,name : e.target.value})))}/>
+                  <input
+                    type="text"
+                    id="name"
+                    placeholder="name"
+                    required
+                    onChange={(e) =>
+                      setConfig((pre) => ({ ...pre, name: e.target.value }))
+                    }
+                  />
                 </div>
                 <div className="form-group">
-                  <label className="connect" htmlFor="name">Connect to DataBase (<img src={Mongo} alt="mongoDB" style={{height:"60px"}}/>)</label>
-                  <input type="text" id="name" placeholder="database url" required onChange={(e)=>(setConfig((pre)=>({...pre,name : e.target.value})))}/>
+                  <label className="connect" htmlFor="name">
+                    Connect to DataBase (
+                    <img src={Mongo} alt="mongoDB" style={{ height: "60px" }} />
+                    )
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    placeholder="database url"
+                    required
+                    onChange={(e) =>
+                      setConfig((pre) => ({ ...pre, dbUrl: e.target.value }))
+                    }
+                    disabled={config.name.length === 0}
+                  />
                 </div>
                 <div className="create-project-gr-btn">
-                  <button
-                    type="submit"
-                  >
-                    Connect
+                  <button type="submit">
+                    {config.isLoading ? "Creating..." : "Create"}
                   </button>
                   <button
                     type="button"
