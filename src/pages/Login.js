6ff7fc1994a5a85e98a0ec904d1e5661ba06d4c7
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-10 12:43:42 +0530

    Add login page icons and update login form layout

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,34 +1,53 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-
-
+import MongoDb from '../assets/mongodb.svg'
+import Api from '../assets/api.svg'
+import Code from '../assets/code.svg'
 function Login() {
-const navigate = useNavigate();
+  const navigate = useNavigate();
   return (
-  <>
-   <div className="login">
-   <div class="background">
-        <div class="shape"></div>
-        <div class="shape"></div>
-    </div>
-    <form>
-        <h3>Login Here</h3>
+    <>
+      <div className="login">
+        <div className="login-icon-container top-left">
+            <img src={MongoDb} alt="MongoDb" className="login-icons" />
+        </div>
+        <div className="login-icon-container top-left-2">
+            <img src={Api} alt="Api" className="login-icons" />
+        </div>
+        <div className="login-icon-container top-left-3">
+            <img src={Code} alt="Code" className="login-icons" />
+        </div>
+        <div className="login-container">
+          <div className="login-logo">
+            <img
+              src="https://th.bing.com/th/id/R.2c7a5258f3b656da08a7b7a021c300f0?rik=dFK6yoQbTVOvDw&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fanimation-png-animation-png-hd-png-image-1008.png&ehk=0m4RVHcY0EzfQU9eezWhk5fJp1Y7EfsJ9mKjRFoZQYk%3d&risl=&pid=ImgRaw&r=0"
+              alt="logo"
+            />
+          </div>
+          <form>
+            <h3>Login Here</h3>
 
-        <label for="username">Username</label>
-        <input type="text" placeholder="Email or Phone" id="username" />
+            <label for="username">Username</label>
+            <input type="text" placeholder="Email or Phone" id="username" />
 
-        <label for="password">Password</label>
-        <input type="password" placeholder="Password" id="password" />
+            <label for="password">Password</label>
+            <input type="password" placeholder="Password" id="password" />
 
-        <button>Log In</button>
-        {/* <div class="social">
-          <div class="go"><i class="fab fa-google"></i>  Google</div>
-          <div class="fb"><i class="fab fa-facebook"></i>  Facebook</div>
-        </div> */}
-    </form>
-    </div>
-  </>
-   
+            <button>Log In</button>
+            <p  onClick={() => {
+                  navigate("/signup");
+                }} className="question">
+              Don't have an account?{" "}
+              <span
+               
+              >
+                Sign Up
+              </span>
+            </p>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
 
