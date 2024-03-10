d35900fa2132dfa74f9108bad475b0bab7a7ce31
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-10 14:03:46 +0530

    Add SignUp functionality and styling

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,33 +1,81 @@
-import React from "react";
+import React,{useState} from "react";
 import { useNavigate } from "react-router-dom";
-
+import MongoDb from '../assets/mongodb.svg'
+import Api from '../assets/api.svg'
+import Code from '../assets/code.svg'
+import { SignUpApi } from "../api/post/SignUpApi";
+import {Error} from '../apiRes/ErrorCatch'
 
 function SignUp() {
-const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [config, setConfig] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    try{
+      const response = await SignUpApi(config);
+      console.log(response);
+      navigate("/login");
+    }catch(e){ 
+      Error(e);
+    }
+  }
   return (
-    <div className="loginContainer">
-      <div className="form-login">
-        <h1>Sign Up</h1>
-        <form>
-          <div className="form-group">
-            <label for="email">Email</label>
-            <input type="email" id="email" />
+    <>
+      <div className="login">
+        <div className="login-icon-container top-left">
+            <img src={MongoDb} alt="MongoDb" className="login-icons" />
+        </div>
+        <div className="login-icon-container top-left-2">
+            <img src={Api} alt="Api" className="login-icons" />
+        </div>
+        <div className="login-icon-container top-left-3">
+            <img src={Code} alt="Code" className="login-icons" />
+        </div>
+        <div className="login-container">
+          <div className="login-logo">
+            <img
+              src="https://th.bing.com/th/id/R.2c7a5258f3b656da08a7b7a021c300f0?rik=dFK6yoQbTVOvDw&riu=http%3a%2f%2fpluspng.com%2fimg-png%2fanimation-png-animation-png-hd-png-image-1008.png&ehk=0m4RVHcY0EzfQU9eezWhk5fJp1Y7EfsJ9mKjRFoZQYk%3d&risl=&pid=ImgRaw&r=0"
+              alt="logo"
+            />
           </div>
-          <div className="form-group">
+          <form onSubmit={(e)=>{handleSignUp(e)}}>
+            <h3>SignUp Here</h3>
+
+            <label for="username">Username</label>
+            <input type="text" placeholder="Email or Phone" id="username" onChange={(e)=>(setConfig((pre)=>(
+              {
+                ...pre,
+                username:e.target.value
+              }
+            )))}/>
+
             <label for="password">Password</label>
-            <input type="password" id="password" />
-          </div>
-          <div className="actionBtn">
+            <input type="password" placeholder="Password" id="password" onChange={(e)=>(setConfig((pre)=>(
+              {
+                ...pre,
+                password:e.target.value
+              }
+            )))}/>
+
             <button type="submit">Sign Up</button>
-          </div>
-          <div className="question">
-            <p>
-              already have an account? <button onClick={() => navigate('/login')}>Login</button>
+            <p  onClick={() => {
+                  navigate("/login");
+                }} className="question">
+              Already have an account?{" "}
+              <span
+               
+              >
+                Login
+              </span>
             </p>
-          </div>
-        </form>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
