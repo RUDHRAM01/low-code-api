4acbd70dbfdfd69760e8a44cec2f860c22e1b886
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-11 22:22:54 +0530

    Refactor App.js to include SideBar and ToastAlert components

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { Route, Routes } from "react-router-dom";
-import Login from "./pages/Login";
-import './styles/login.css'
-import './styles/admin.css'
-import SignUp from "./pages/SignUp";
-import AdminDashboard from "./pages/AdminDashboard";
+import React from "react";
+import SideBar from "./components/common/SideBar";
+import { BrowserRouter as Router } from "react-router-dom";
+import ToastAlert from "./apiRes/ToastAlert";
 
 function App() {
+
   return (
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
+    <>
+      <Router>
+        <SideBar />
+        <ToastAlert />
+      </Router>
+    </>
   );
 }
 
