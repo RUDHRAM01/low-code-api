cae7fab5502574765b13885b5614a5bb29933d00
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-11 22:23:18 +0530

    Refactor SideBar component to include conditional rendering and add RouterConfig

--- src/components/common/SideBar.js
+++ src/components/common/SideBar.js
@@ -1,22 +1,58 @@
-import React from 'react'
-import Home from '../../assets/home.svg'
-import Analytics from '../../assets/analytics.svg'
-import Settings from '../../assets/settings.svg'
-import Logo from '../../assets/logo.jpg'
+import React, { useEffect } from "react";
+import Home from "../../assets/home.svg";
+import Analytics from "../../assets/analytics.svg";
+import Settings from "../../assets/settings.svg";
+import Logo from "../../assets/logo.jpg";
+import RouterConfig from "../../routes/RouterConfig";
+
 function SideBar() {
+  const [isShow, setIsShow] = React.useState(true);
+  useEffect(() => {
+    if (
+      window.location.pathname === "/login" ||
+      window.location.pathname === "/sign-up" ||
+      window.location.pathname === "/"
+    ) {
+      setIsShow(false);
+    } else {
+      setIsShow(true);
+    }
+  }, []);
+
   return (
-    <div className='side-bar'>
-        <div className='logo'>
-            <img src={Logo} alt='logo' height={40} style={{borderRadius:"50%"}}/>
-            <span>Low code api</span>
-        </div>
-       <div className='container-side'>
-       <div className='side-bar-item'><img src={Home} alt='home' height={24}/>Home</div>
-        <div className='side-bar-item'><img src={Analytics} alt='Analytics' height={24}/>Analytics</div>
-        <div className='side-bar-item'><img src={Settings} alt='settigs' height={24}/>Settings</div>
-       </div>
-    </div>
-  )
+    <>
+      <div className="base">
+        {isShow && (
+          <div className="side-bar">
+            <div className="logo">
+              <img
+                src={Logo}
+                alt="logo"
+                height={40}
+                style={{ borderRadius: "50%" }}
+              />
+              <span>Low code api</span>
+            </div>
+            <div className="container-side">
+              <div className="side-bar-item">
+                <img src={Home} alt="home" height={24} />
+                Home
+              </div>
+              <div className="side-bar-item">
+                <img src={Analytics} alt="Analytics" height={24} />
+                Analytics
+              </div>
+              <div className="side-bar-item">
+                <img src={Settings} alt="settigs" height={24} />
+                Settings
+              </div>
+            </div>
+          </div>
+        )}
+        <RouterConfig />
+      </div>
+    </>
+  );
 }
 
-export default SideBar
+export default SideBar;
