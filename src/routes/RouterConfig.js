77921b3eda69d10d2d6aec0c0032502905cd1185
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2024-03-15 16:45:12 +0530

    Add ViewProject route to RouterConfig

--- src/routes/RouterConfig.js
+++ src/routes/RouterConfig.js
@@ -3,12 +3,14 @@ import { Route, Routes } from "react-router-dom";
 import AdminDashboard from "../pages/AdminDashboard";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
+import ViewProject from "../components/ViewProject";
 
 function RouterConfig() {
   return (
     <>
       <Routes>
         <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/admin/:id" element={<ViewProject />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
