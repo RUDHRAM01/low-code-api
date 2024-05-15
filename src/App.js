5489d59f5360368dffcaadd167b2ecfcde08361d
Rudhram Saraswat <rudhramsaraswat2770@gmail.com>
2024-05-15 17:00:01 +0530

    chore: Add CSS file for App component styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SideBar from "./components/common/SideBar";
 import { BrowserRouter as Router } from "react-router-dom";
 import ToastAlert from "./apiRes/ToastAlert";
+import './App.css';
 
 function App() {
 
