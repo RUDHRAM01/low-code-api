6767519bea353d6641726993bb74b27388382c68
Rudhram Saraswat <rudhramsaraswat2770@gmail.com>
2024-05-20 20:47:22 +0530

    feat: Add delete confirmation modal to ViewApi component

--- src/components/ViewApi.js
+++ src/components/ViewApi.js
@@ -6,12 +6,15 @@ import deleteIcon from "../assets/delete.svg";
 import copyIcon from "../assets/copy.svg";
 import searchIcon from "../assets/searchB.svg";
 import { getSchemaNameApi } from "../api/get/GetSchemaNameApi";
+import toast from "react-hot-toast";
+import { deleteApi } from "../api/delete/DeleteApi";
 
 function ViewApi({ project }) {
   const navigate = useNavigate();
   const [schema, setSchema] = useState([]);
   const { id } = useParams();
   const [userApi, setApis] = useState([]);
+  const [deleteIndex, setDeleteIndex] = useState(null);
   const calculateHeight = () => {
     const viewApi = document.querySelector(".view-project-process");
     const height = viewApi?.offsetHeight;
@@ -47,7 +50,30 @@ function ViewApi({ project }) {
     };
     calling();
   }, [id]);
+
+  const copytoclipboard = (text) => { 
+    navigator.clipboard.writeText(text)
+    .then(() => {
+      toast.success('Copied to clipboard');
+    })
+    .catch((err) => {
+      toast.error('Failed to copy to clipboard');
+    });
+  }
+
+  const handleDeleteApi = async () => {
+    try {
+      await deleteApi(deleteIndex);
+      toast.success("API Deleted Successfully");
+      const res = await getApi(id);
+      setApis(res.data.message);
+      setDeleteIndex(null);
+    } catch (error) {
+      Error(error);
+    }
+  }
   return (
+    <>
     <div className="view-api-container">
       <div className="view-header">
         <h4>API's</h4>
@@ -95,8 +121,14 @@ function ViewApi({ project }) {
                   </div>
                 </div>
                 <div className="space-between">
-                  <img src={deleteIcon} alt="edit" height={"24px"} />
-                  <img src={copyIcon} alt="edit" height={"24px"} />
+                  <img src={deleteIcon} alt="edit" height={"24px"} style={{cursor:"pointer"}}
+                  data-bs-toggle="modal"
+                  data-bs-target="#exampleModal"
+                  onClick={() => setDeleteIndex(item._id)}
+                  />
+                  <img src={copyIcon} alt="edit" style={{cursor:"pointer"}} height={"24px"} onClick={()=>{
+                    copytoclipboard(`${process.env.REACT_APP_MAIN}/${project.name}/${id}/${item.endpoint}${(item.method === "DELETE" || item.method === "PUT") ? '/:id' : ''}`)
+                  }}/>
                 </div>
               </div>
               <div className="singleApi">
@@ -115,6 +147,50 @@ function ViewApi({ project }) {
         })}
       </div>
     </div>
+      <div
+      class="modal fade"
+      id="exampleModal"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h1 class="modal-title fs-5" id="exampleModalLabel">
+              Confirmation
+            </h1>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            Are you sure you want to delete this API?
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              Close
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary"
+              onClick={handleDeleteApi}
+              style={{ background: "red", border: "none" }}
+            >
+              delete
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    </>
   );
 }
 
